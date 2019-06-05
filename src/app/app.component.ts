a7c6cf246375bf6444e38273c48d11c3fba28ce8
Alex Severino <alex@7lab.nl>
2019-06-05 10:59:21 +0200

    Angular reactive forms

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { resolve } from 'q';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    genders = ['male', 'female'];
+    signupForm: FormGroup;
+    forbiddenUsernames = ['Chris', 'Anna'];
+
+    ngOnInit() {
+        this.signupForm = new FormGroup({
+            'userData': new FormGroup({
+                'username': new FormControl('Panda', [Validators.required, this.forbiddenNames.bind(this)]),
+                'email': new FormControl('Bear ', [Validators.required, Validators.email], this.forbiddenEmails),
+            }),
+              'gender': new FormControl('male', Validators.required),
+              'hobbies': new FormArray([])
+        });
+    }
+
+    onSubmit() {
+        console.log(this.signupForm);
+    }
+
+    onAddHobby() {
+        const control = new FormControl(null, Validators.required);
+        (<FormArray>this.signupForm.get('hobbies')).push(control);
+    }
+
+    forbiddenNames(control: FormControl): {[s: string]: boolean} {
+        if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+            return {'nameIsForbidden': true};
+        } else {
+            return null;
+        }
+    }
+
+    forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+        const promise = new Promise<any>((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === 'test@test.com') {
+                    resolve({'emailIsForbidden': true});
+                } else {
+                    resolve(null);
+                }
+            }, 1500);
+        });
+        return promise
+    }
+}
